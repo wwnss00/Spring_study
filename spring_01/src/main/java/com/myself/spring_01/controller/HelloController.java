99bd20942527c5ede0eda3dcd13596a6a56336ab
wwnss00 <wns208500@gmail.com>
2025-06-11 00:51:15 +0900

    회원관리예제

---
+++ spring_01/src/main/java/com/myself/spring_01/controller/HelloController.java
@@ -0,0 +1,51 @@
+package com.myself.spring_01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    // 웹 애플리케이션에서 /hello라고 들어오면 이 메서드를 호출
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    //쓸 일 거의 없음
+    @GetMapping("hello-stirng")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
---
+++ spring_01/src/main/java/com/myself/spring_01/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.myself.spring_01.repository;
+
+import com.myself.spring_01.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
