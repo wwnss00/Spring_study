e369e24c0e1e901d9e66131d6ec1c021e7f270e1
wwnss00 <wns208500@gmail.com>
2025-06-11 19:44:13 +0900

    회원 관리 예제

---
+++ spring_01/src/main/java/com/myself/spring_01/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package com.myself.spring_01.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
