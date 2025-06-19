527b5b5dd20b759cda374e2e61c20a6372df3fdc
wwnss00 <wns208500@gmail.com>
2025-06-19 17:22:58 +0900

    회원 도메인 실행과 테스트

--- core/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -6,11 +6,11 @@ public class MemberServiceImpl implements MemberService{
 
     @Override
     public void join(Member member) {
-
+        memberRepository.save(member);
     }
 
     @Override
     public Member findMember(Long memberId) {
-        return null;
+        return memberRepository.findById(memberId);
     }
 }
