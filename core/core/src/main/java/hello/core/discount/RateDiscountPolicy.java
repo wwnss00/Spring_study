f78444b2d33590da412e93cef6934ce4e422608d
wwnss00 <wns208500@gmail.com>
2025-06-19 23:07:34 +0900

    새로운 할인 정책 적용과 문제점

---
+++ core/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int prince) {
+        if (member.getGrade() == Grade.VIP) {
+            return prince * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
